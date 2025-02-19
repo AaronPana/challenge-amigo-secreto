ca141d13a565dbce55178bc02f7e356aca4335d6
Aaron Pana <aaronpana02@gmail.com>
2025-02-19 12:08:53 -0300

    feat: sortear amigo de la lista

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación.
 // Aquí deberás desarrollar la lógica para resolver el problema.
 
+// * Constantes
 const friends = [];
 
 const friendNameInput = document.getElementById("amigo");
 const addFriendButton = document.getElementsByClassName("button-add")[0];
+const chooseFriendButton = document.getElementsByClassName("button-draw")[0];
 const friendsList = document.getElementById("listaAmigos");
+const resultList = document.getElementById("resultado");
 
+//  * Limpiar listas
 friendsList.innerHTML = "";
+resultList.innerHTML = "";
 
+// * Funciones para el boton añadir
 const validateInput = () => {
 	if (friendNameInput.value === "") {
 		alert("Por favor, inserte un nombre.");
@@ -27,8 +33,30 @@ const addFriend = () => {
 	if (!validateInput()) return;
 	friends.push(friendNameInput.value);
 	addListItem(friendNameInput.value);
-	console.log({ friends });
 	friendNameInput.value = "";
 };
 
+// * Evento para el boton añadir
 addFriendButton.addEventListener("click", addFriend);
+
+// * Funciones para el boton seleccionar
+const validateListContent = () => {
+	if (friends.length === 0) {
+		alert("No hay amigos registrados.");
+		return false;
+	}
+	return true;
+};
+
+const generateRandomNumber = (max) => {
+	return Math.floor(Math.random() * max);
+};
+
+const chooseFriend = () => {
+	if (!validateListContent()) return;
+	const randomIndex = generateRandomNumber(friends.length);
+	resultList.innerHTML = `<li>${friends[randomIndex]}</li>`;
+};
+
+// * Evento para el boton seleccionar
+chooseFriendButton.addEventListener("click", chooseFriend);
